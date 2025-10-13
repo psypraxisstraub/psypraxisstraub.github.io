b5bd1f04761b44bc6c8bfba9abd142d81a89530e
chrishaider <christian.haider@fh-hagenberg.at>
2025-10-13 17:38:06 +0200

    Apply changes for neueröffnung

---
+++ _posts/2025-10-13-neueroeffnung.md
@@ -0,0 +1,27 @@
+---
+layout: post
+lang: de
+title: "Praxisneueröffnung ab Herbst 2025"
+description: Neueröffnung der Praxis im Herbst 2025
+keywords: Eröffnung
+date: 2025-03-24 17:00 +0200
+periodprefix: Anmeldungen ab
+perioddate: 2025-10-01 17:00 +0200
+categories:
+  - "Neues"
+author: Sofia
+image: /blog/resources/2025-10-13-neueroeffnung/logo.PNG
+---
+
+Ab Herbst 2025 eröffnet Frau Sofia Straub, MSc., ihre neue Praxis für klinische Psychologie und DBT in Freistadt. Mit einer langjährigen Erfahrung als Skillstrainerin und Caregiver für psychosoziale Notfallhilfe freut sich Frau Straub darauf, ihre Expertise in einem modernen und einladenden Umfeld anzubieten.
+
+<!--more-->
+
+Die Praxis richtet sich an Menschen, die Unterstützung in Krisenzeiten benötigen oder langfristige psychologische Begleitung suchen. Ab sofort sind noch Termine verfügbar – sichern Sie sich rechtzeitig einen Platz und profitieren Sie von einem maßgeschneiderten Therapieansatz, der Ihnen hilft, Ihre persönlichen Ziele zu erreichen.
+
+<br />
+<div class="post-closing">
+Auf Ihr Kommen freut sich Frau Sofia Straub, MSc.!<br />
+Klinische Psychologin, Skillstrainerin nach DBT, Caregiver für psychosoziale Notfallhilfe
+</div>
+<br />
--- assets/js/custom/map.js
+++ assets/js/custom/map.js
@@ -2,7 +2,7 @@
    var map = L.map('map', {
        dragging: !L.Browser.mobile,
        tap: !L.Browser.mobile
-   }).setView([48.36936535443323, 14.513704089483294], 5);
+   }).setView([48.51936535443323, 14.613704089483294], 5);
 
    // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
    //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -20,10 +20,10 @@
    map.scrollWheelZoom.disable();
 
 
-   var marker = L.marker([48.304732, 14.284464056011316]).addTo(map)
-       .bindPopup('<b>Praxis Sofia Straub</b><br/>Altstadt 22<br/>Wohnung 5<br/>4020 Linz'
-           + '<br/><br/><a target="_blank" href="https://www.openstreetmap.org/directions?from=&to=48.304776%2C14.284474#map=19/48.304764/14.284126">Route (via OpenStreetMap)</a>'
-           + '<br/><a target="_blank" href="https://www.google.at/maps/place/Altstadt+22,+4020+Linz/@48.3046669,14.2816891,729m/data=!3m2!1e3!4b1!4m6!3m5!1s0x47739783bf26b2ff:0x96e52eec858d4630!8m2!3d48.3046634!4d14.284264!16s%2Fg%2F11c5b7m3pn?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D">Route (via Google Maps)</a>');
+   var marker = L.marker([48.515137, 14.493681]).addTo(map)
+       .bindPopup('<b>Praxis Sofia Straub</b><br/>Sonnbergstraße 27<br/>4240 Freistadt'
+           + '<br/><br/><a target="_blank" href="https://www.openstreetmap.org/search?query=Sonnbergstra%C3%9Fe+27%2C+4240+Freistadt&zoom=19&minlon=14.49063688516617&minlat=48.51430524272826&maxlon=14.495786726474764&maxlat=48.5158865556798#map=19/48.515096/14.493681">Route (via OpenStreetMap)</a>'
+           + '<br/><a target="_blank" href="https://www.google.at/maps/place/Sonnbergstra%C3%9Fe+27,+4240+Freistadt/@48.5151007,14.4910935,747m/data=!3m2!1e3!4b1!4m6!3m5!1s0x47730b76c90a4559:0x4d4024ceeb5f9487!8m2!3d48.5150972!4d14.4936684!16s%2Fg%2F11csf80zrg?entry=ttu&g_ep=EgoyMDI1MTAwOC4wIKXMDSoASAFQAw%3D%3D">Route (via Google Maps)</a>');
 
    marker.openPopup();
    // var popup = L.popup()
